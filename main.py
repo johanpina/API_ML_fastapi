573ba5720513887ac022ebb49ff8dea0e8361d3b
johanpina <johanpina@MacBook-Pro-de-NECSOFT.local>
2023-07-08 11:41:03 -0500

    Mi primer API de ML

---
+++ main.py
@@ -0,0 +1,26 @@
+from  fastapi import FastAPI
+import pandas as pd
+import pickle
+
+
+# Cargamos el modelo previamente entrenado
+with open('models/modelGB.pkl', 'rb') as Gb:
+    modelo = pickle.load(Gb)
+
+
+app = FastAPI()
+
+@app.get('/')
+def hello():
+    return {'message': 'Hello World'}
+
+@app.post('/predict')
+def predict(request:dict):
+    # Get the data from the POST request.
+    data = request['data']
+    # Make prediction using model loaded from disk as per the data.
+    prediction = modelo.predict(data)
+    # Take the first value of prediction
+    output = prediction[0]
+
+    return {'prediction': output}
